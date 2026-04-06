64bb289ee05c3da184630cea9e39b06efa2ab666
JoelBBzao <cbsiqueira@id.uff.br>
2026-04-06 14:46:10 -0300

    MesDoAnoOtimizado.py

--- MesDoAnoOtimizado.py
+++ MesDoAnoOtimizado.py
@@ -1,5 +1,5 @@
 mes = int(input('Insira o número a ser analisado: '))
-meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Juho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 num = 0 + mes
 if mes <=0 or mes >=13:
     print('Não existe um mês correspondente á esse número!')
