2cfd385f1368088bae2204aee434c8a92eab396a
JoelBBzao <cbsiqueira@id.uff.br>
2026-04-06 14:44:49 -0300

    Add files via upload
    Tentei fazer de uma forma mais otimizada que coma menos bytes

---
+++ MesDoAnoOtimizado.py
@@ -0,0 +1,8 @@
+mes = int(input('Insira o número a ser analisado: '))
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Juho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+num = 0 + mes
+if mes <=0 or mes >=13:
+    print('Não existe um mês correspondente á esse número!')
+else:
+    num = mes
+    print(f'O número analisado corresponde ao mês de {meses[num-1]}')
