9be8ead29eab7f93088ea7cd03b1f9f5552ad660
JoelBBzao <cbsiqueira@id.uff.br>
2026-04-18 15:08:26 -0300

    ListaDe5NumerosPositivos.py

--- ListaDe5NumerosPositivos.py
+++ ListaDe5NumerosPositivos.py
@@ -1,21 +1,15 @@
-listaA = []
-listaB = []
-listaC = []
-
+lista = []
 for x in range(5):
-    num = int(input('Digite um número para A: '))
-    listaA.append(num)
-print(listaA)
-
-for x in range(5):
-    num = int(input('Digite um número para B: '))
-    listaB.append(num)
-print(listaB)
-listaC.extend(listaA)
-listaC.extend(listaB)
-for x in (listaC):
-    while p < len(listaC) and num > listaC[p]:
-        p += 1
-    listaC.insert(p, listaC[x])
-print(listaC)
-
+    num = int(input('Digite um número: '))
+    if num <= -1:
+        print('insira números positivos')
+    elif num == 0:
+        break
+    else:
+        lista.append(num)
+if len(lista) == 5:
+    for i in range(5):
+        print(f'{lista[i]}')
+    lista.clear()
+else:
+    print(lista)
